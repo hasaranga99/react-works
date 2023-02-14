824487251a53090e6590dc0df8c2cd89fd240393
Hasaranga <rvndhasaranga@gmail.com>
2023-02-14 16:08:58 +0530

    external mainblock

--- prac1/src/App.js
+++ prac1/src/App.js
@@ -1,37 +1,20 @@
 import './App.css';
 import Main from './Components/Main';
 
-const myData = [
-  {
-    name : 'Ravindu',
-    city : 'medirigiriya',
-    Position : 'web development',
-  },
-  {
-    name : 'hasaranga',
-    city : 'colombo',
-    Position : 'Software engineer',
-  },
-  {
-    name : 'kavishka',
-    city : 'Polonnaruwa',
-    Position : 'React',
-  },
-  {
-    name : 'dilshan',
-    city : 'maharagama',
-    Position : 'node js',
-  },
-]
+
+import { myData } from './Data/myData';
+
+const MainBlock = myData.map(({name,city,Position,id})=> {
+  return <Main key={id} name={name} city={city} Position={Position}/>;
+})
+
 
 function App() {
   return (
   <div>
     Hasaranga Learning center
     <h1>Ravindu Hasaranga</h1>
-    {myData.map(({name,city,Position})=> {
-      return <Main name={name} city={city} Position={Position}/>;
-    })}
+    {MainBlock}
   </div>
   );
 }
---
+++ prac1/src/Data/myData.js
@@ -0,0 +1,26 @@
+export const myData = [
+  {
+    id : 123,
+    name : 'Ravindu',
+    city : 'medirigiriya',
+    Position : 'web development',
+  },
+  {
+    id : 456,
+    name : 'hasaranga',
+    city : 'colombo',
+    Position : 'Software engineer',
+  },
+  {
+    id : 778,
+    name : 'kavishka',
+    city : 'Polonnaruwa',
+    Position : 'React',
+  },
+  {
+    id  : 912,
+    name : 'dilshan',
+    city : 'maharagama',
+    Position : 'node js',
+  },
+]
